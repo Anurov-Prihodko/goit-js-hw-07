96642239147cf88a1df62b6d81428fdd1cb226ee
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-05-21 13:39:01 +0300

    1 задача

--- js/task-01.js
+++ js/task-01.js
@@ -14,14 +14,24 @@
 Количество элементов: 4
 */
 
-const sumCategoriesEl = document.querySelector('#categories');
+
 const itemEl = document.querySelectorAll('.item');
-const titleEl = document.querySelectorAll('.item h2');
+const AmountOfElementsAnimals = document.querySelectorAll('#categories > li:nth-child(1) > ul');
+const AmountOfElementsProducts = document.querySelectorAll('#categories > li:nth-child(2) > ul');
+const AmountOfElementsTechnologies = document.querySelectorAll('#categories > li:nth-child(3) > ul');
+const animalsValue = document.querySelectorAll('#categories > li:nth-child(1) > h2');
+const productsValue = document.querySelectorAll('#categories > li:nth-child(2) > h2');
+const technologiesValue = document.querySelectorAll('#categories > li:nth-child(3) > h2');
 
-// function getFirstHomeWork {
-    
-// }
-titleEl.forEach(animal => console.log(animal.textContent))
+console.log(`В списке ${itemEl.length} категории`)
 
-// console.log(animal);
+animalsValue.forEach(el => console.log(`Категория: ${el.textContent}`));
+AmountOfElementsAnimals.forEach(el => console.log(`Количество элементов: ${el.children.length}`));
+
+productsValue.forEach(el => console.log(`Категория: ${el.textContent}`));
+AmountOfElementsProducts.forEach(el => console.log(`Количество элементов: ${el.children.length}`));
+
+technologiesValue.forEach(el => console.log(`Категория: ${el.textContent}`));
+AmountOfElementsTechnologies.forEach(el => console.log(`Количество элементов: ${el.children.length}`));
+
 
