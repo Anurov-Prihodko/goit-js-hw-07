3126080b38a5915fa6d243e93c6ae1dad5ed9f9b
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-05-21 13:41:25 +0300

    1

--- js/task-01.js
+++ js/task-01.js
@@ -16,9 +16,9 @@
 
 
 const itemEl = document.querySelectorAll('.item');
-const AmountOfElementsAnimals = document.querySelectorAll('#categories > li:nth-child(1) > ul');
-const AmountOfElementsProducts = document.querySelectorAll('#categories > li:nth-child(2) > ul');
-const AmountOfElementsTechnologies = document.querySelectorAll('#categories > li:nth-child(3) > ul');
+const amountOfElementsAnimals = document.querySelectorAll('#categories > li:nth-child(1) > ul');
+const amountOfElementsProducts = document.querySelectorAll('#categories > li:nth-child(2) > ul');
+const amountOfElementsTechnologies = document.querySelectorAll('#categories > li:nth-child(3) > ul');
 const animalsValue = document.querySelectorAll('#categories > li:nth-child(1) > h2');
 const productsValue = document.querySelectorAll('#categories > li:nth-child(2) > h2');
 const technologiesValue = document.querySelectorAll('#categories > li:nth-child(3) > h2');
@@ -26,12 +26,12 @@ const technologiesValue = document.querySelectorAll('#categories > li:nth-child(
 console.log(`В списке ${itemEl.length} категории`)
 
 animalsValue.forEach(el => console.log(`Категория: ${el.textContent}`));
-AmountOfElementsAnimals.forEach(el => console.log(`Количество элементов: ${el.children.length}`));
+amountOfElementsAnimals.forEach(el => console.log(`Количество элементов: ${el.children.length}`));
 
 productsValue.forEach(el => console.log(`Категория: ${el.textContent}`));
-AmountOfElementsProducts.forEach(el => console.log(`Количество элементов: ${el.children.length}`));
+amountOfElementsProducts.forEach(el => console.log(`Количество элементов: ${el.children.length}`));
 
 technologiesValue.forEach(el => console.log(`Категория: ${el.textContent}`));
-AmountOfElementsTechnologies.forEach(el => console.log(`Количество элементов: ${el.children.length}`));
+amountOfElementsTechnologies.forEach(el => console.log(`Количество элементов: ${el.children.length}`));
 
 
