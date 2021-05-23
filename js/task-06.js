15961da8612c9a1c14e58590ad053995d6bc9182
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-05-23 12:05:37 +0300

    6 задача

---
+++ js/task-06.js
@@ -0,0 +1,47 @@
+/*
+Напиши скрипт, который бы при потере фокуса на инпуте,
+ проверял его содержимое на правильное количество символов.
+
+<input
+  type="text"
+  id="validation-input"
+  data-length="6"
+  placeholder="Введи 6 символов"
+/>
+Сколько символов должно быть в инпуте, указывается в его атрибуте
+ data-length.
+Если введено подходящее количество, 
+то border инпута становится зеленым, если неправильное - красным.
+Для добавления стилей, используй CSS-классы valid и invalid.
+
+#validation-input {
+  border: 3px solid #bdbdbd;
+}
+
+#validation-input.valid {
+  border-color: #4caf50;
+}
+
+#validation-input.invalid {
+  border-color: #f44336;
+}
+*/
+
+const inputEl = document.querySelector('input#validation-input');    
+const lengthOfInput = inputEl.getAttribute('data-length');
+
+inputEl.addEventListener('blur', onInputValidation);
+
+function onInputValidation() {    
+    if (inputEl.value.length === Number(lengthOfInput)) {
+        inputEl.classList.add('valid')
+        inputEl.classList.remove('invalid')
+    } if (inputEl.value.length !== Number(lengthOfInput)) {
+        inputEl.classList.add('invalid')
+        inputEl.classList.remove('valid')
+    } if (inputEl.value.length === 0) {
+        inputEl.classList.remove('valid')
+        inputEl.classList.remove('invalid')
+    }
+};
+
